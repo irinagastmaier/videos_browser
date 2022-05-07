e27b4c18a8393fd7260897c709224dc7a5a735ae
irinagastmaier <irinagmarques@hotmail.com>
2022-05-07 22:46:32 +0200

    reponsive styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,5 +2,5 @@ import { Image } from '@chakra-ui/react';
 import img from '../assets/images/Netflix-Background.jpg';
 
 export default function Home() {
-  return <Image src={img} alt='movies_bg' w='100%' h='auto' />;
+  return <Image src={img} alt='movies_bg'  w='100%' h='87vh'   objectFit='cover' />;
 }
