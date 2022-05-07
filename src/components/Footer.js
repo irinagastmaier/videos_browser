d73f8ae377ead0f9315fb0ceb3f81c20ea56e7e8
irinagastmaier <irinagmarques@hotmail.com>
2022-05-07 22:20:48 +0200

    landing page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,14 @@ export default function Footer() {
   const year = new Date().getFullYear();
   return (
     <>
-      <footer style={{ textAlign: 'center', color: '#79cadc' }}>
+      <footer
+        style={{
+          textAlign: 'center',
+          color: '#79cadc',
+          background: 'black',
+          padding: '0.5rem',
+        }}
+      >
         &copy;{year} Irina Gastmaier
       </footer>
     </>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,6 @@
+import { Image } from '@chakra-ui/react';
+import img from '../assets/images/Netflix-Background.jpg';
+
 export default function Home() {
-  return <div>Home</div>;
+  return <Image src={img} alt='movies_bg' w='100%' h='auto' />;
 }
