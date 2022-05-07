b4e3fb582abd63584318cb1566fc47a1252e974e
irinagastmaier <irinagmarques@hotmail.com>
2022-05-07 21:34:12 +0200

    card

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,5 +1,35 @@
+import {
+  Box,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+
 export default function NotFound() {
   return (
-    <div>NotFound</div>
-  )
+    <Box textAlign="center" py={10} px={6}>
+      <Heading
+        display="inline-block"
+        as="h2"
+        size="2xl"
+        bgGradient="linear(to-r, blue.300, blue.500)"
+        backgroundClip="text"
+      >
+        404
+      </Heading>
+      <Text fontSize="18px" mt={3} mb={2} color='gray.100'>
+        Page Not Found
+      </Text>
+      <Link to="/">
+        <Button
+          colorScheme="white"
+          variant="solid"
+        >
+          Go to Home
+        </Button>
+      </Link>
+    </Box>
+  );
 }
